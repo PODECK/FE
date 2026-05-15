dc2e86eddd0fe6668c98c60d599616ab2bfcbacd
안성진 <anseongjin@anseongjin-ui-MacBookPro.local>
2026-05-15 19:57:25 +0900

    Design: 트레이너 상태바 UI 조정 (#28)

--- src/app/(main)/home/_components/TrainerStatusBar.tsx
+++ src/app/(main)/home/_components/TrainerStatusBar.tsx
@@ -16,7 +16,7 @@ export default function TrainerStatusBar({
   battleRecord,
 }: TrainerStatusBarProps) {
   return (
-    <section className="relative z-20 mx-auto flex h-16 w-[100%] items-center justify-between rounded-lg bg-[#444444] px-8 text-[var(--color-secondary-2)] shadow-[0_10px_28px_rgba(0,0,0,0.18)]">
+    <section className="relative z-20 mx-auto flex h-16 w-[100%] items-center justify-between rounded-[15px] bg-[#444444] px-8 text-[var(--color-secondary-2)] shadow-[0_10px_28px_rgba(0,0,0,0.18)]">
       <div className="flex items-center gap-3 text-lg font-medium">
         <Typewriter
           options={{
@@ -29,19 +29,22 @@ export default function TrainerStatusBar({
         />
       </div>
 
-      <div className="flex items-center gap-5 text-base text-[#EAEAEA]">
+      <div className="flex items-center gap-1 text-base text-[#EAEAEA]">
         <div className="relative">
-          {/* <span className="absolute -top-10 left-1/2 -translate-x-1/2 rounded-lg bg-[var(--color-primary)] px-4 py-2 text-sm font-bold text-white shadow-md">
-            열 수 있는 카드팩이 있어요!
-          </span> */}
-          <span>보유 카드팩 {cardPackCount} 개</span>
+          <span className="text-[#E0E0E0]">
+            보유 카드팩 <strong className="text-[#FFF]">{cardPackCount}</strong> 개
+          </span>
         </div>
-        <span>|</span>
+        <span className="pl-[3px] text-[#C0C0C0]">|</span>
         <span className="h-4 w-px bg-[#444444]" />
-        <span>탑 진행도 {towerProgress} 층</span>
-        <span>|</span>
-        <span className="h-4 w-px bg-[#444444]" />
-        <span>배틀 전적 {battleRecord}</span>
+        <span className="text-[#E0E0E0]">
+          탑 진행도 <strong className="text-[#FFF]">{towerProgress}</strong> 층
+        </span>
+        <span className="pl-[3px] text-[#C0C0C0]">|</span>
+        <span className="h-4 bg-[#444444]" />
+        <span className="text-[#E0E0E0]">
+          배틀 전적 <strong className="text-[#FFF]">{battleRecord}</strong>
+        </span>
       </div>
     </section>
   );
