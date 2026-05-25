33a60c6bee3cf29b902123e626a4f2126b8dc52f
seongmin36 <whtjdals3625@gmail.com>
2026-05-25 22:14:34 +0900

    feat(r3f): finalize BattleCanvas component assembly

--- src/features/battle/game/r3f/BattleCanvas.tsx
+++ src/features/battle/game/r3f/BattleCanvas.tsx
@@ -16,6 +16,7 @@ export default function BattleCanvas() {
   return (
     <Canvas className="absolute inset-0" gl={{ antialias: true, alpha: true }}>
       <BattleCamera />
+      <ScreenShake />
       <Suspense fallback={null}>
         <ambientLight intensity={0.6} />
         <directionalLight position={[5, 5, 5]} intensity={0.8} />
@@ -27,7 +28,6 @@ export default function BattleCanvas() {
         <AttackParticles />
         <HitEffect />
       </Suspense>
-      <ScreenShake />
       <EffectComposer>
         <Bloom intensity={0.4} luminanceThreshold={0.7} luminanceSmoothing={0.3} mipmapBlur />
       </EffectComposer>
